75aa408f153bd183ca9f7dc97b650a3b7d738af0
Taqtile <taqtile@Way.local>
2018-11-27 19:55:43 -0200

    adicionado hello world

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));

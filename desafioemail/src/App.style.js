0c1ebb9eba9654d9180bc229a6872b47a28a4fda
Taqtile <taqtile@Way.local>
2018-12-01 16:34:49 -0200

    adicionado o issue do email

---
+++ desafioemail/src/App.style.js
@@ -0,0 +1,55 @@
+
+export const Style = {
+    general: {
+        height: '100%',
+        width: '100%',
+        padding: '10%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'grid',
+        gridTemplateColumns: 'auto , auto , auto',
+        gridTemplateRows: 'auto, auto , auto',
+        gridGap:'50px',
+        gridColumn: '1/3',
+        justifySelf: 'center',
+        alignItens: 'end',
+        backgroundColor: '#fffff0',
+        zIndex: '1',
+        margin: 'auto',
+
+
+    },
+    btn: {
+        height: '100%',
+        color: 'white',
+        backgroundColor: '#b22222',
+        fontSize: '15px',
+        padding: '8px',
+        width: '60%',
+        borderRadius: '10%',
+        zIndex: '2',
+
+    },
+    box: {
+        height: '30px',
+        width: '200px',
+        padding: '10px',
+        boxSizing: 'border-box',
+        border: 'light-blue',
+        backgroundColor: '#fffff0', 
+        zIndex: '2',  
+        margin: 'auto'    
+    },
+    warning: {
+        height: '100%',
+        color: 'red',
+        
+    } ,
+    labels:{
+        color: 'black',
+        fontFamily:'sans-serif',
+        justifySelf: 'center',
+        alignItems: 'center',
+        
+    },
+};
